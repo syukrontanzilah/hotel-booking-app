1c2cafe0d8709f22b955d352e6fae16363406bdc
syukrontanzilah <syukrontanzilah@gmail.com>
2024-06-27 08:34:00 +0700

    add logout link

--- app/components/UserNav.tsx
+++ app/components/UserNav.tsx
@@ -8,24 +8,51 @@ import { MenuIcon } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react';
 import UserImg from '../../public/user.png'
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import {
+    RegisterLink, 
+    LoginLink, 
+    LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
-const UserNav = () => {
+const UserNav = async () => {
+    const {getUser} = getKindeServerSession();
+    const user = await getUser()
   return (
     <DropdownMenu>
         <DropdownMenuTrigger asChild>
             <div className='rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3'>
                 <MenuIcon className='w-6 h-6 lg:w-5 lg:h-5'/>
-                <Image src={UserImg} alt='user-img' className='rounded-full w-8 h-8 lg:block'/>
+                <img 
+                src={
+                    user?.picture ??
+                    '/user.png'
+                } 
+                alt='user-img' 
+                className='rounded-full w-8 h-8 lg:block'/>
             </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent align='end' className='w-[200px]'>
-            <DropdownMenuItem>
-                <RegisterLink className='w-full'>Register</RegisterLink>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-                <LoginLink className='w-full'>Login</LoginLink>
-            </DropdownMenuItem>
+            {
+                user?
+                (
+                <DropdownMenuItem>
+                <LogoutLink className='w-full'>Logout</LogoutLink>
+                </DropdownMenuItem>
+                )
+                :
+                (
+                <>
+                <DropdownMenuItem>
+                    <RegisterLink className='w-full'>Register</RegisterLink>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <LoginLink className='w-full'>Login</LoginLink>
+                </DropdownMenuItem>
+                </>                    
+                )
+            }
+
         </DropdownMenuContent>
     </DropdownMenu>
   )
